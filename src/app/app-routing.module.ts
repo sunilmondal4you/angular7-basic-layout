8fc333f0abe27cec7c26ce76e7e666556cd017b1
sunilmondal4you <sunilmondal4you@gmail.com>
2019-09-16 12:39:24 +0530

    about component added...

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './about/about.component';
 
 const routes: Routes = [
   { path: '',         redirectTo:'./home/home.component', pathMatch: 'full' },
-  { path: 'home',     component: HomeComponent, pathMatch: 'full' },
-  { path: 'about',    component: HomeComponent, pathMatch: 'full' },
-  { path: 'contactus',component: HomeComponent, pathMatch: 'full' },
+  { path: 'home',     component: HomeComponent,  pathMatch: 'full' },
+  { path: 'about',    component: AboutComponent, pathMatch: 'full' },
+  { path: 'contactus',component: HomeComponent,  pathMatch: 'full' },
 ];
 
 @NgModule({
