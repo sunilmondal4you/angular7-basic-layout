4d6ff59c693c2299a22422611ca6761fdd0d8fd6
sunilmondal4you <sunilmondal4you@gmail.com>
2019-09-16 13:47:53 +0530

    navbar content move to header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { AboutComponent } from './components/about/about.component';
 import { ContactusComponent } from './components/contactus/contactus.component';
 
 const routes: Routes = [
-  { path: '',         redirectTo:'./home/home.component', pathMatch: 'full' },
+  { path: '',         component: HomeComponent, pathMatch: 'full' },
   { path: 'home',     component: HomeComponent,      pathMatch: 'full' },
   { path: 'about',    component: AboutComponent,     pathMatch: 'full' },
   { path: 'contactus',component: ContactusComponent, pathMatch: 'full' },
