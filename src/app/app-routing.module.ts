a4b331b2037cbf49caee17cb39cf11e6b98c8a35
sunilmondal4you <sunilmondal4you@gmail.com>
2019-09-16 12:40:09 +0530

    cotact us page added.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './about/about.component';
+import { ContactusComponent } from './contactus/contactus.component';
 
 const routes: Routes = [
   { path: '',         redirectTo:'./home/home.component', pathMatch: 'full' },
-  { path: 'home',     component: HomeComponent,  pathMatch: 'full' },
-  { path: 'about',    component: AboutComponent, pathMatch: 'full' },
-  { path: 'contactus',component: HomeComponent,  pathMatch: 'full' },
+  { path: 'home',     component: HomeComponent,      pathMatch: 'full' },
+  { path: 'about',    component: AboutComponent,     pathMatch: 'full' },
+  { path: 'contactus',component: ContactusComponent, pathMatch: 'full' },
 ];
 
 @NgModule({
